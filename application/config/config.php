e8a68dd160a686c52a3847716a6d700d083c7d82
irwan-12 <irwan.lavaindonesia@gmail.com>
2021-03-11 15:37:16 +0700

    pembaharuan cpanel

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://localhost/lavaresto/';
-$config['base_url'] = 'http://lavaresto.com/';
+$config['base_url'] = 'http://localhost/lavaresto/';
+// $config['base_url'] = 'http://lavaresto.com/';
 
 /*
 |--------------------------------------------------------------------------
